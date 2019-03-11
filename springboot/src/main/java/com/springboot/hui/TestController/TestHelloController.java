cf25749296ff88f4a06b8550ca1321d20a055902
chunhb <@travelsky.com>
2019-03-11 17:41:35 +0800

    输出日志，暂时这个test的配置是只输出到文件中，控制台不会输出，下一版本配置控制台同步输出

--- springboot/src/main/java/com/springboot/hui/TestController/TestHelloController.java
+++ springboot/src/main/java/com/springboot/hui/TestController/TestHelloController.java
@@ -1,14 +1,19 @@
 package com.springboot.hui.TestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.rmi.runtime.Log;
 
 @RestController
 public class TestHelloController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestHelloController.class);
+
     @RequestMapping(value = "/testHello")
     public String testHello(){
-
+        LOGGER.info("this is test info",this.getClass());
         return "hello springboot";
     }
 
