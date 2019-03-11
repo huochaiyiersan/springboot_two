5c76ea5cf6db10b84a83a7dec0cf728d1eae8738
chunhb <@travelsky.com>
2019-03-11 10:24:39 +0800

    初始化springboot

---
+++ springboot/src/main/java/com/springboot/hui/HuiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.hui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuiApplication.class, args);
+    }
+
+}
