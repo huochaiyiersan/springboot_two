e1d41a48111ed174847043742f84f11265bcf0eb
chunhb <@travelsky.com>
2019-03-13 16:40:49 +0800

    整合 Freemarker

---
+++ springboot/src/main/java/com/springboot/hui/Controller/freemarker/FreemarkerController.java
@@ -0,0 +1,18 @@
+package com.springboot.hui.Controller.freemarker;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("freemarker")
+public class FreemarkerController {
+
+    @RequestMapping("hello")
+    public String hello(Map<String,Object> map) {
+
+        map.put("msg", "Hello Freemarker");
+        return "hello";
+    }
+}
