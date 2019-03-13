371978befc6b95e47f5d24ad641fb972f592a034
chunhb <@travelsky.com>
2019-03-13 10:49:57 +0800

    属性装配，从xml配置中获取数据

--- springboot/src/main/java/com/springboot/hui/HuiApplication.java
+++ springboot/src/main/java/com/springboot/hui/HuiApplication.java
@@ -1,13 +1,18 @@
 package com.springboot.hui;
 
+import com.springboot.hui.configbean.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HuiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HuiApplication.class, args);
+        ConfigurableApplicationContext action = SpringApplication.run(HuiApplication.class, args);
+
+        WebConfig webConfig = action.getBean(WebConfig.class);
+        webConfig.show();
     }
 
 }
---
+++ springboot/src/main/java/com/springboot/hui/configbean/WebConfig.java
@@ -0,0 +1,22 @@
+package com.springboot.hui.configbean;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class WebConfig {
+
+    @Value("${ds.userName}")
+    private String userName;
+
+    @Autowired
+    private Environment environment;
+
+    public void show() {
+        System.out.println("ds.userName:" + this.userName);
+        System.out.println("ds.password:" + this.environment.getProperty("ds.password"));
+    }
+}
