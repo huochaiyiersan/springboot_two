061c7628b209ad0e43b1c2204afdff7491184e08
chunhb <@travelsky.com>
2019-03-14 17:25:57 +0800

    整合拦截器，spring的，已过时，

---
+++ springboot/src/main/java/com/springboot/hui/configbean/InterceptorConfig.java
@@ -0,0 +1,20 @@
+package com.springboot.hui.configbean;
+
+import com.springboot.hui.Interceptor.TimeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class InterceptorConfig extends WebMvcConfigurerAdapter {
+    @Autowired
+    private TimeInterceptor timeInterceptor;
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(timeInterceptor);
+    }
+
+}
