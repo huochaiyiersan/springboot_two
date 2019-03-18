75eaebeb0da5e99e27c9542932d58ca854b53f25
chunhb <@travelsky.com>
2019-03-18 11:05:31 +0800

    使用spring自带的定时器（改变注解位置一样效果）

--- springboot/src/main/java/com/springboot/hui/timer/springtime/Schedule.java
+++ springboot/src/main/java/com/springboot/hui/timer/springtime/Schedule.java
@@ -1,11 +1,13 @@
 package com.springboot.hui.timer.springtime;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
+@EnableScheduling//spring自带定时器要加这个注解即可
 public class Schedule {
 
     @Scheduled(fixedRate = 2000)//代表每2秒执行一次
