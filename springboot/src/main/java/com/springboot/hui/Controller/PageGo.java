2c731425b547df7c4e955d693d57e8a2f9881cdb
chunhb <@travelsky.com>
2019-03-18 08:36:02 +0800

    整合邮件，（配置文件密码已删除）补充漏提交内容，新加一个控制层做页面跳转测试用

---
+++ springboot/src/main/java/com/springboot/hui/Controller/PageGo.java
@@ -0,0 +1,10 @@
+package com.springboot.hui.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "pagego")
+public class PageGo {
+
+}
